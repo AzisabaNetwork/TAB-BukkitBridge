a5df5ce83f33d78be303ea172bb5a3ddbd7c6d02
acrylic-style <me@acrylicstyle.xyz>
2022-01-04 00:43:15 +0900

    Fix NPE

--- src/main/java/net/azisaba/tabBukkitBridge/data/providers/VaultDataProvider.java
+++ src/main/java/net/azisaba/tabBukkitBridge/data/providers/VaultDataProvider.java
@@ -7,17 +7,19 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 
+import java.util.Objects;
+
 public class VaultDataProvider {
     public static void register() {
         if (Bukkit.getPluginManager().isPluginEnabled("Vault")) {
-            Permission perm = Bukkit.getServicesManager().getRegistration(Permission.class).getProvider();
-            Chat chat = Bukkit.getServicesManager().getRegistration(Chat.class).getProvider();
-            Economy economy = Bukkit.getServicesManager().getRegistration(Economy.class).getProvider();
-            DataKey.PRIMARY_GROUP.register(Util.isPluginEnabledPredicate("Vault"), perm::getPrimaryGroup);
-            DataKey.PREFIX.register(Util.isPluginEnabledPredicate("Vault"), chat::getPlayerPrefix);
-            DataKey.SUFFIX.register(Util.isPluginEnabledPredicate("Vault"), chat::getPlayerSuffix);
-            DataKey.MONEY.register(Util.isPluginEnabledPredicate("Vault"), economy::getBalance);
-            DataKey.MONEY_FORMATTED.register(Util.isPluginEnabledPredicate("Vault"), p -> Util.formatWithCommas(economy.getBalance(p)));
+            Permission perm = Bukkit.getServicesManager().load(Permission.class);
+            Chat chat = Bukkit.getServicesManager().load(Chat.class);
+            Economy economy = Bukkit.getServicesManager().load(Economy.class);
+            DataKey.PRIMARY_GROUP.register(p -> perm != null, p -> Objects.requireNonNull(perm).getPrimaryGroup(p));
+            DataKey.PREFIX.register(p -> chat != null, p -> Objects.requireNonNull(chat).getPlayerPrefix(p));
+            DataKey.SUFFIX.register(p -> chat != null, p -> Objects.requireNonNull(chat).getPlayerSuffix(p));
+            DataKey.MONEY.register(p -> economy != null, p -> Objects.requireNonNull(economy).getBalance(p));
+            DataKey.MONEY_FORMATTED.register(p -> economy != null, p -> Util.formatWithCommas(Objects.requireNonNull(economy).getBalance(p)));
         }
     }
 }
